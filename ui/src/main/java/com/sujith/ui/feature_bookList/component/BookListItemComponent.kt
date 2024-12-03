2b70c72c86709bb39e1da11a74b58a9c174b3415
Sujith PS <ps.sujith@outlook.com>
2024-12-03 13:30:06 +0530

    UI tweaks

--- ui/src/main/java/com/sujith/ui/feature_bookList/component/BookListItemComponent.kt
+++ ui/src/main/java/com/sujith/ui/feature_bookList/component/BookListItemComponent.kt
@@ -17,10 +17,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -30,18 +26,17 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import com.sujith.domain.feature_bookList.model.BookItem
 import com.sujith.ui.R
 import com.sujith.ui.resource.card_view_card_elevation
 import com.sujith.ui.resource.card_view_card_elevation_image
-import com.sujith.ui.resource.extra_small_font_size
 import com.sujith.ui.resource.list_item_height
 import com.sujith.ui.resource.medium_font_size
 import com.sujith.ui.resource.padding_extra_small
 import com.sujith.ui.resource.padding_large
 import com.sujith.ui.resource.padding_medium
+import com.sujith.ui.resource.small_font_size
 import com.sujith.ui.utils.CoilImage
 import com.sujith.ui.utils.ItemUtil
 
@@ -114,7 +109,7 @@ fun BookListItemComponent(
                         vertical = padding_extra_small
                     )
                     .fillMaxWidth(),
-                fontSize = extra_small_font_size,
+                fontSize = small_font_size,
                 maxLines = 3,
                 color = MaterialTheme.colorScheme.onBackground,
                 fontWeight = FontWeight.ExtraBold,
